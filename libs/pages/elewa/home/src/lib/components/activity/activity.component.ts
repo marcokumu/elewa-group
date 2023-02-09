1955acf1a27099a6062821376851985607b57f9d
marcokumu <marcokumu10@gmail.com>
2023-02-09 08:42:23 +0000

    ✨feat: files from the created component i.e activity component

---
+++ libs/pages/elewa/home/src/lib/components/activity/activity.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-activity',
+  templateUrl: './activity.component.html',
+  styleUrls: ['./activity.component.scss'],
+})
+export class ActivityComponent {}
