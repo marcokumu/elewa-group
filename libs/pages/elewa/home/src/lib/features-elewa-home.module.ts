ad9d3f3da155331860c2e3f22c06008154dad9bf
marcokumu <marcokumu10@gmail.com>
2023-02-09 08:29:21 +0000

    ✨feat: imported the ActivityComponent

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
+import { ActivityComponent } from './components/activity/activity.component';
+import { CardsModule } from '@elewa-group/features/components/cards';
+// import { ActivityComponent } from './components/activity/activity.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [HomePageComponent, HomeHeroSectionComponent],
-  exports: [HomePageComponent, HomeHeroSectionComponent],
+  imports: [CommonModule, CardsModule],
+  declarations: [HomePageComponent, HomeHeroSectionComponent, ActivityComponent],
+  exports: [HomePageComponent, HomeHeroSectionComponent, ActivityComponent],
 })
 export class HomePageModule {}
